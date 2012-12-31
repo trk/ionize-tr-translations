53e4aa320de8fa5bf5ce77cd02bc51fa9bbcb143
Iskender <iskender@6ve1.com>
2012-12-31 17:48:28 +0200

    Yönetim paneli ve bazı modül çevirileri
    - Yönetim Paneli
    - RSS Modülü
    - Search Modülü

---
+++ modules/Rss/language/tr/rss_lang.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['module_rss_about'] = '
+	Bu modül, modüle eklenen sayfa makalelerinden RSS beslemesi oluşturuyor.<br/>
+	<b>Önemli : </b>Makalelerin <b>"aranabilir"</b> seçeneğinin işaretli olması gerekiyor.<br/>
+	İçeriği olmayan makaleler listelenmeyecek.<br/>
+	<br/>
+	Bağlantı URL leri "dil" seçenkli ve seçeneksiz aşağıdaki gibidir :
+';
+
+// Text strings
+$lang['module_rss_label_feed_title'] 			= 'Besleme Başlığı';
+$lang['module_rss_label_feed_description'] 		= 'Besleme Açıklaması';
+$lang['module_rss_label_feed_author'] 			= 'Yazar E-posta adresi (isteğe bağlı)';
+$lang['module_rss_settings'] 					= 'RSS Besleme Ayarları';
+$lang['module_rss_pages'] 						= 'RSS Sayfaları';
+$lang['module_rss_error_writing_config_file'] 	= 'modules/Rss/config/config.php dosyasına yazılamıyor !';
+
+/* End of file rss_lang.php */
+/* Location: /modules/Rss/language/en/rss_lang.php */
